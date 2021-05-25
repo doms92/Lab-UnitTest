ddcbdea9265c4c71ed9b8d383e0a4ea0eeac430e
dom summers <summers8734@student.cptc.edu>
2021-05-24 18:19:05 -0700

    Fixed Course Class so Test will be able to run

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,9 +6,14 @@ namespace BusinessLogic
 {
     public class Course
     {
+        string defaultInstructName = "STAFF";
+        private byte numberOfCredits;
+        private string courseName;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = defaultInstructName;
         }
 
         /// <summary>
@@ -19,12 +24,41 @@ namespace BusinessLogic
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName
+        {
+            get => CourseName;
+            set
+            {
+                if(value == string.Empty)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    courseName = value;
+                };
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
+        public byte NumberOfCredits
+        {
+            get => numberOfCredits;
+            set
+            {
+                if (value <= 30 && value > 0)
+                {
+                    numberOfCredits = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                };
+                
+            }
+        }
 
     }
 }
